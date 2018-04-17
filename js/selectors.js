48d1d7a765001ca1dc6b37abd68399c831d11520
marcantoineandreoli@gmail.com Andreoli <marcantoineandreoli@gmail.com>
2018-04-17 01:21:23 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,10 +1,8 @@
 // declare your functions here...
-$('.pics')
-
 function paragraphSelector() {
-  return $('p')
+  return $('p');
 }
 
 function lastImageSelector {
-  return $('div .pics ul:last')
+  return $('div .pics ul:last');
 }
